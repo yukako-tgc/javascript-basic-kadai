1c081aa434f1a1c8682e651bf67a24e0a8e20041
yukako-tgc <159108142+yukako-tgc@users.noreply.github.com>
2024-02-07 10:28:44 +0900

    kadai005 の再提出

---
+++ kadai_005再提出/bmi.js
@@ -0,0 +1,35 @@
+// 変数の宣言
+let height;
+
+// 値の代入
+height = '身長';
+
+// 変数の宣言・値の代入
+let  heightNumber = 1.7;
+
+ // コンソールへの出力
+ console.log(height);
+ console.log(heightNumber);
+
+ // 変数の宣言
+ let weight;
+
+//値の代入
+weight = `体重`;
+
+// 変数の宣言・値の代入
+let weightNumber = 68;
+
+// コンソールへの出力
+console.log(weight);
+console.log(weightNumber);
+
+// 変数の宣言
+let bmi;
+
+// 値の代入
+bmi = 'BMI';
+
+//BMI計算
+console.log(bmi)
+console.log(bmi = weightNumber / (heightNumber * heightNumber));
