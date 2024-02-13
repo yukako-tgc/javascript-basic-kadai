ab0b61187e2be199c731b64d74c518de6b943fc3
yukako-tgc <159108142+yukako-tgc@users.noreply.github.com>
2024-02-13 19:47:52 +0900

    課題020

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,11 @@
 // btnというidを持つHTML要素を取得し、定数に代入する
 const btn = document.getElementById('btn');
 
+// textというidを持つHTML要素を取得し、定数に代入する
+const textElement = document.getElementById('text');
+
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', () => {
   // ボタンがクリックされたら、テキストを書き換える
-  document.getElementById('text').innerHTML = 'ボタンがクリックされました！';
+  textElement.innerHTML = 'ボタンがクリックされました！';
 });
